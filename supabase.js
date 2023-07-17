cb431e740d616d00f4d41c13eb81c26128b0b55c
Tim <timdobranski@gmail.com>
2023-07-16 22:52:59 -0700

    Migrated from expo constants to .env

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 // import 'react-native-url-polyfill/auto';
-import Constants from 'expo-constants';
-const { SUPABASE_URL, SUPABASE_PUBLIC_API_KEY } = Constants.manifest.extra;
+// import Constants from 'expo-constants';
+// const { SUPABASE_URL, SUPABASE_PUBLIC_API_KEY } = Constants.manifest.extra;
 import * as SecureStore from 'expo-secure-store'
 import { createClient } from '@supabase/supabase-js';
-
+import { SUPABASE_API_KEY, SUPABASE_URL } from "@env";
 
 const ExpoSecureStoreAdapter = {
   getItem: (key) => {
@@ -18,8 +18,8 @@ const ExpoSecureStoreAdapter = {
 }
   // Supabase handling
 
-  console.log('supabase: ', SUPABASE_PUBLIC_API_KEY, SUPABASE_URL)
-  const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_API_KEY, {
+  console.log('supabase: ', SUPABASE_API_KEY, SUPABASE_URL)
+  const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY, {
     auth: {
       storage: ExpoSecureStoreAdapter,
       autoRefreshToken: true,
